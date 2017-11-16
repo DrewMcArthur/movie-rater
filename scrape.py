fbf96600fa5c61c970627624b8e484badcc171ad
Drew McArthur <drewmcarthur1@gmail.com>
2017-11-16 16:08:33 +0100

    add more scrape files for sandboxing

--- scrape.py
+++ scrape.py
@@ -3,3 +3,18 @@
     
     Defines functions that scrape movie data from various databases
 """
+
+import urllib.request
+
+# IMSDb
+
+uri = "http://www.imsdb.com/all%20scripts/"
+
+f = urllib.request.urlopen(uri)
+html = f.read()
+f.close()
+
+print(html)
+
+# TODO parse html
+# ISSUE scripts are mostly drafts. 
