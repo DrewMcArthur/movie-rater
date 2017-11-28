56c20ecccd86415991faf7b6479fdf829c64d2c2
Drew McArthur <drewmcarthur1@gmail.com>
2017-11-28 13:30:00 +0100

    update filterHeaders function

--- clean_data.py
+++ clean_data.py
@@ -7,7 +7,6 @@
 
 def filterHeaders(row):
     """ given a row (dict), remove unnecessary columns from data """
-    data = {}
     goodHeaders = [ 'adult', 'budget', 'genres', 'overview', 'title', 
                     'popularity', 'production_companies', 'release_date', 
                     'production_countries', 'runtime', 'spoken_languages', 
@@ -17,9 +16,8 @@ def filterHeaders(row):
                     'type', 'dvd', 'boxoffice', 'production' ]
 
     # filter out useless information
-    for key, val in row.items():
-        if key in goodHeaders:
-            data.update({key: val})
+    data = {key: val for key, val in row.items() 
+            if key in goodHeaders}
 
     return data
 
